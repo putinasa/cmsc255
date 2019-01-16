b527dc4fde4cc8f11e7b1c66e8e96a7e6f1c40d9
putinasa <46756981+putinasa@users.noreply.github.com>
2019-01-16 12:11:34 -0500

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,24 @@
+
+/*******************************
+* Hello.World
+********************************
+*Java test
+*Armandas Putinas
+*1/16/2019
+*255 and 004
+*******************************/
+public class Hello {
+	public static void printHeading() {
+		System.out.println("Your Name");
+		System.out.println("Java Project Name");
+		System.out.println("Project Description:");
+		System.out.println("Version Date");
+		System.out.println("Course Number and Section:");
+		System.out.println();
+	}
+	
+	public static void main(String [] args) {
+		printHeading();
+		System.out.println("Hello");
+	}
+}
